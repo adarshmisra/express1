67648ce677c3a2f8150b86f0d29355ec841bb52a
Adarsh <adarshmisra98@gmail.com>
2018-09-25 12:44:14 +0530

    First version

---
+++ models/author.js
@@ -0,0 +1,32 @@
+var mongoose=require('mongoose');
+
+var moment=require('moment');
+var Schema=mongoose.Schema;
+var AuthorSchema=new Schema(
+    {
+        first_name:{type:String,required:true,max:100},
+        family_name: {type: String, required: true, max: 100},
+        date_of_birth: {type: Date},
+        date_of_death: {type: Date},
+    }
+)
+AuthorSchema.virtual('name').get(function()
+{
+    return this.family_name+ ', '+this.first_name;
+});
+AuthorSchema.virtual('lifespan').get(function()
+{
+return moment(this.date_of_death_formatted.getYear()-this,date_of_birth_formatted.getYear()).format('MMMM Do, YYYY').toString();
+});
+
+AuthorSchema.virtual('url').get(function(){
+    return '/catalog/author/'+this._id;
+});
+AuthorSchema.virtual('date_of_birth_formatted').get(function(){
+return moment(this.date_of_birth).format('MMMM Do, YYYY');
+});
+
+AuthorSchema.virtual('date_of_death_formatted').get(function(){
+    return moment(this.date_of_death).format('MMMM Do, YYYY');
+    });
+module.exports=mongoose.model('Author',AuthorSchema);
